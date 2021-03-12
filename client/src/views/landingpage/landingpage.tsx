c833f04d7f80050caa97c94c07576bdfa0fc1094
aadarshjha <jha.aadarsh@gmail.com>
2021-03-12 14:02:48 -0600

    just testing some things out

---
+++ client/src/views/landingpage/landingpage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography, Button } from "antd";
+const { Title } = Typography;
+
+const LandingPage: React.FC<{}> = () => (
+  <div>
+      <Typography>
+          <Title>
+              Welcome To VUMS!
+          </Title>
+      </Typography>
+  </div>
+);
+
+export default LandingPage;
