9806b5a6aad57761c2b88ad82e8c4f10c8cf80e5
aadarshjha <jha.aadarsh@gmail.com>
2021-03-13 11:39:38 -0600

    placeholder image

--- client/src/views/landingpage/Scroll.tsx
+++ client/src/views/landingpage/Scroll.tsx
@@ -3,13 +3,21 @@ import "antd/dist/antd.css";
 import { Divider } from 'antd';
 import "./landingpage.css"
 
+const leftMilestoneData = [
+  
+]
+
 const Scroll: React.FC<{}> = () => (
   <div className="scroll">
     <div className="left">
-      test
+      <div>
+        {
+
+        }
+      </div>
     </div>
     <div>
-      <Divider type="vertical" style={{backgroundColor: 'black', width: '3px'}}/>
+      <Divider type="vertical" style={{backgroundColor: 'black', width: '2px'}}/>
     </div>
     <div className="right">
       test
