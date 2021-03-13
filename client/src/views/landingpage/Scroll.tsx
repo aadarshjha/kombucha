fe84fc959f76a24d9f452a24f596ebfd73c6e6be
aadarshjha <jha.aadarsh@gmail.com>
2021-03-13 11:47:51 -0600

    template map function

--- client/src/views/landingpage/Scroll.tsx
+++ client/src/views/landingpage/Scroll.tsx
@@ -3,24 +3,47 @@ import "antd/dist/antd.css";
 import { Divider } from 'antd';
 import "./landingpage.css"
 
-const leftMilestoneData = [
-  
-]
+
+// I guess we can fetch this from some backend that we build. 
+// this is all mocked dummy data. 
+const leftMilestoneData = {
+  "Event #1": ['./placeholder.png', 'Some Text to describe the event.'],
+  "Event #2": ['./placeholder.png', 'Some Text to describe the event.'],
+  "Event #3": ['./placeholder.png', 'Some Text to describe the event.']
+}
+
+const rightMiletoneData = {
+  "Event #1": ['./placeholder.png', 'Some Text to describe the event.'],
+  "Event #2": ['./placeholder.png', 'Some Text to describe the event.'],
+  "Event #3": ['./placeholder.png', 'Some Text to describe the event.']
+}
 
 const Scroll: React.FC<{}> = () => (
   <div className="scroll">
     <div className="left">
-      <div>
-        {
-
-        }
-      </div>
+        {Object.entries(leftMilestoneData).map(([key, value]) => {
+          return (
+            <div>
+              <h1>{key}</h1>
+              <img src={value[0]} alt=""/>
+              <p>{value[1]}</p>
+            </div>
+          );
+        })}
     </div>
     <div>
       <Divider type="vertical" style={{backgroundColor: 'black', width: '2px'}}/>
     </div>
     <div className="right">
-      test
+    {Object.entries(rightMiletoneData).map(([key, value]) => {
+          return (
+            <div>
+              <h1>{key}</h1>
+              <img src={value[0]} alt=""/>
+              <p>{value[1]}</p>
+            </div>
+          );
+        })}
     </div>
   </div>
 );
