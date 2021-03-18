2021bc81835a0e40a7a2dca09e787efa092a5fed
aadarshjha <jha.aadarsh@gmail.com>
2021-03-18 16:26:44 -0500

    working on cats / easy

--- client/src/views/learn/category.tsx
+++ client/src/views/learn/category.tsx
@@ -1,14 +1,36 @@
 import React from "react";
 
+// or import types. FIXME in a optimzation / re write. 
+type backendData = {
+    header: string;
+    body: string;
+    difficulty: string;
+    category: string;
+};
+
 type catProps = {
   difficulty: string;
-  categories: Array<string>;
+  categories: Array<backendData>;
 };
 
 const Category: React.FC<catProps> = ({ difficulty, categories }: catProps) => {
   return (
+    //   we can demonstrate all easy, medium, hard categories
     <div>
-      <h1>hello world</h1>
+        <h1>
+            {difficulty}
+        </h1>
+        {
+            // categories.map(element => {
+            //     return (
+            //         <div>
+            //             <p>
+            //                 element.catego
+            //             </p>
+            //         </div>
+            //     )
+            // })
+        }
     </div>
   );
 };
--- client/src/views/learn/learn.tsx
+++ client/src/views/learn/learn.tsx
@@ -2,59 +2,94 @@ import React from "react";
 import Logo from "../../components/Logo";
 import Category from "./category";
 
-// FETCHING THE DATA WILL GO HERE! 
+// FETCHING THE DATA WILL GO HERE!
 // we can simple filter here
 
-const dataFromBackend = [
+type backendData = {
+  header: string;
+  body: string;
+  difficulty: string;
+  category: string;
+};
+
+const dataFromBackend: Array<backendData> = [
   {
-    "header": "This is the title of the article", 
-    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
-    "difficulty": "easy",
-    "category": "research"
+    header: "This is the title of the article",
+    body:
+      "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    difficulty: "easy",
+    category: "research",
   },
   {
-    "header": "This is the title of the article", 
-    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
-    "difficulty": "medium",
-    "category": "test1"
+    header: "This is the title of the article",
+    body:
+      "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    difficulty: "medium",
+    category: "test1",
   },
   {
-    "header": "This is the title of the article", 
-    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
-    "difficulty": "hard",
-    "category": "test2"
+    header: "This is the title of the article",
+    body:
+      "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    difficulty: "hard",
+    category: "test2",
   },
   {
-    "header": "This is the title of the article", 
-    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
-    "difficulty": "easy",
-    "category": "test3"
+    header: "This is the title of the article",
+    body:
+      "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    difficulty: "easy",
+    category: "test3",
   },
   {
-    "header": "This is the title of the article", 
-    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
-    "difficulty": "medium",
-    "category": "test4"
+    header: "This is the title of the article",
+    body:
+      "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    difficulty: "medium",
+    category: "test4",
   },
   {
-    "header": "This is the title of the article", 
-    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
-    "difficulty": "hard",
-    "category": "test5"
-  }
-]
+    header: "This is the title of the article",
+    body:
+      "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    difficulty: "hard",
+    category: "test5",
+  },
+];
+
+const fetchCategories = () => {
+  const easy: Array<backendData> = [];
+  const medium: Array<backendData> = [];
+  const hard: Array<backendData> = [];
+  dataFromBackend.map((element) => {
+    if (element.category === "easy") {
+      easy.push(element);
+    } else if (element.category === "medium") {
+      medium.push(element);
+    } else {
+      hard.push(element);
+    }
+  });
+  return {
+    easy: easy,
+    medium: medium,
+    hard: hard,
+  };
+};
 
 const Learn: React.FC<Record<string, never>> = () => {
+  const data = fetchCategories()
   return (
     <div>
       <Logo page="Learn With VUMS" />
       <div>
+        {/* Basically,  */}
         {/* easy */}
-        <Category />
+        <Category difficulty={"easy"} categories={data.easy} />
         {/* medium */}
-        <Category />
+        <Category difficulty={"medium"} categories={data.medium} />
         {/* hard */}
-        <Category />
+        <Category difficulty={"hard"} categories={data.hard} />
       </div>
     </div>
   );
