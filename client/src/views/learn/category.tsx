a22984755b62ec82e492e69f13830d7164b57ae5
aadarshjha <jha.aadarsh@gmail.com>
2021-03-18 15:36:46 -0500

    mocking up some data

--- client/src/views/learn/category.tsx
+++ client/src/views/learn/category.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Category: React.FC<Record<string, never>> = () => {
+type catProps = {
+  difficulty: string;
+  categories: Array<string>;
+};
+
+const Category: React.FC<catProps> = ({ difficulty, categories }: catProps) => {
   return (
     <div>
-        <h1>hello world</h1>
-    </div>    
-  )
+      <h1>hello world</h1>
+    </div>
+  );
 };
 
 export default Category;
--- client/src/views/learn/learn.tsx
+++ client/src/views/learn/learn.tsx
@@ -2,15 +2,62 @@ import React from "react";
 import Logo from "../../components/Logo";
 import Category from "./category";
 
+// FETCHING THE DATA WILL GO HERE! 
+// we can simple filter here
+
+const dataFromBackend = [
+  {
+    "header": "This is the title of the article", 
+    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    "difficulty": "easy",
+    "category": "research"
+  },
+  {
+    "header": "This is the title of the article", 
+    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    "difficulty": "medium",
+    "category": "test1"
+  },
+  {
+    "header": "This is the title of the article", 
+    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    "difficulty": "hard",
+    "category": "test2"
+  },
+  {
+    "header": "This is the title of the article", 
+    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    "difficulty": "easy",
+    "category": "test3"
+  },
+  {
+    "header": "This is the title of the article", 
+    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    "difficulty": "medium",
+    "category": "test4"
+  },
+  {
+    "header": "This is the title of the article", 
+    "body": "yeet this is some smart text from VUMS that is really smart idk what to write becuase i do not know anything about micribiologyyeetet here i ssomemore text i like some text etc etc yuh this is even more text lets go herere more text yeet.",
+    "difficulty": "hard",
+    "category": "test5"
+  }
+]
+
 const Learn: React.FC<Record<string, never>> = () => {
   return (
     <div>
       <Logo page="Learn With VUMS" />
       <div>
+        {/* easy */}
+        <Category />
+        {/* medium */}
+        <Category />
+        {/* hard */}
         <Category />
       </div>
-    </div>    
-  )
+    </div>
+  );
 };
 
 export default Learn;
