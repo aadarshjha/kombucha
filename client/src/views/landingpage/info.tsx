b308cecb68c0efb0938776e59625f2327300bfdd
aadarshjha <jha.aadarsh@gmail.com>
2021-03-18 12:23:25 -0500

    formatting

--- client/src/views/landingpage/info.tsx
+++ client/src/views/landingpage/info.tsx
@@ -1,62 +1,35 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined } from "@ant-design/icons";
 import "./landingpage.css";
 import { Avatar } from "antd";
 
+const dummyData = [
+  "Asia Miller",
+  "Aadarsh Jha",
+  "Sam Lee",
+  "Ashwin Kumar",
+  "Zeyad Moustafa",
+];
+
 const Info: React.FC<Record<string, never>> = () => (
   <div className="centeredHeader">
-      {/* general information about VUMS */}
-      <h1>
-          Meet The Board
-      </h1>
+    {/* general information about VUMS */}
+    <h1>Meet The Board</h1>
 
-      <div className="avatars">
-          <div>
+    <div className="avatars">
+      {dummyData.map((element) => {
+        return (
+          <div key={element}>
             <Avatar size={128} icon={<UserOutlined />} />
-            <br/>
-            <p>
-                ThisisA Name
-            </p>
+            {/* this is temp (br) dont roast */}
+            <br />
+            <br />
+            <p>{element}</p>
           </div>
-          <div>
-            <Avatar size={128} icon={<UserOutlined />} />
-            <br/>
-            <p>
-                ThisisA Name
-            </p>
-          </div>
-          <div>
-            <Avatar size={128} icon={<UserOutlined />} />
-            <br/>
-            <p>
-                ThisisA Name
-            </p>
-          </div>
-          <div>
-            <Avatar size={128} icon={<UserOutlined />} />
-            <br/>
-            <p>
-                ThisisA Name
-            </p>
-          </div>
-          <div>
-            <Avatar size={128} icon={<UserOutlined />} />
-            <br/>
-            <p>
-                ThisisA Name
-            </p>
-          </div>
-          <div>
-            <Avatar size={128} icon={<UserOutlined />} />
-            <br/>
-            <p>
-                ThisisA Name
-            </p>
-          </div>
-      </div>
-      
-
+        );
+      })}
+    </div>
   </div>
 );
 
