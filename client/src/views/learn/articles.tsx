763c3ee0260f9943331fbdd2b5b54bf4452c6c95
aadarshjha <jha.aadarsh@gmail.com>
2021-03-19 00:45:03 -0500

    checking in more code

--- client/src/views/learn/articles.tsx
+++ client/src/views/learn/articles.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./articles.css";
-import { Card } from "antd";
+import { Card, List } from "antd";
 
 type backendData = {
   header: string;
@@ -14,21 +14,30 @@ type catProps = {
 };
 
 const Articles: React.FC<catProps> = ({ articles }: catProps) => {
+  console.log(">>>");
+  console.log(articles);
   return (
     <div className="flex">
       <div className="left">
-        {/* all of the articles */}
-        {articles.map((element) => {
-          <div key={element.header}>
-            <Card
-              title={element.header}
-              bordered={false}
-              style={{ width: "100%" }}
-            ></Card>
-          </div>;
-        })}
+        {/* all of the articles
+        {articles.map((element, i) => {
+          return (
+          );
+        })} */}
+        <List
+            bordered
+            dataSource={articles}
+            renderItem={item => (
+              <List.Item>
+                  <h4>
+                  {item.header}
+                  </h4>
+              </List.Item>
+            )}
+          />
       </div>
       <div className="right">
+          {/* This is dynamically loaded based on what is selected */}
         <div>{/* header */}</div>
         <div>{/* body */}</div>
       </div>
