ae9db29c18d7ef53385945b765cda58ed56ade24
aadarshjha <jha.aadarsh@gmail.com>
2021-03-18 20:39:27 -0500

    added all the example cards

--- client/src/views/events/events.tsx
+++ client/src/views/events/events.tsx
@@ -1,25 +1,51 @@
 import React from "react";
 import Logo from "../../components/Logo";
 import { Card } from "antd";
+import "./events.css";
+import { title } from "node:process";
 
 const { Meta } = Card;
 
+const eventData = [
+  {
+    title: "Event Title #1",
+    body:
+      "This is an example of an event. It will be a short para and you will be able to type some more stuff into the text.",
+  },
+  {
+    title: "Event Title #2",
+    body:
+      "This is an example of an event. It will be a short para and you will be able to type some more stuff into the text.",
+  },
+  {
+    title: "Event Title #3",
+    body:
+      "This is an example of an event. It will be a short para and you will be able to type some more stuff into the text.",
+  },
+  {
+    title: "Event Title #4",
+    body:
+      "This is an example of an event. It will be a short para and you will be able to type some more stuff into the text.",
+  },
+];
+
 const Events: React.FC<Record<string, never>> = () => {
   return (
-    <div>
+    <div className="centeredCards">
       <Logo page="Events With VUMS" />
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
+
+      {eventData.map((element) => {
+        return (
+          <Card
+            hoverable
+            key={element.title}
+            style={{ width: "40%", marginBottom: "10px" }}
+            cover={<img alt="test image" src="test.jpg" />}
+          >
+            <Meta title={element.title} description={element.body} />
+          </Card>
+        );
+      })}
     </div>
   );
 };
