8d26cbd6a9a1788efaf5652ce70d926a16c9e382
aadarshjha <jha.aadarsh@gmail.com>
2021-03-18 20:40:13 -0500

    fix

--- client/src/views/events/events.tsx
+++ client/src/views/events/events.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Logo from "../../components/Logo";
 import { Card } from "antd";
 import "./events.css";
-import { title } from "node:process";
 
 const { Meta } = Card;
 
