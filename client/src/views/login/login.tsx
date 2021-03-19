2376e8d577607d352fc61bd031782e98f418ee13
aadarshjha <jha.aadarsh@gmail.com>
2021-03-19 12:43:04 -0500

    added a toggle butotn:

--- client/src/views/login/login.tsx
+++ client/src/views/login/login.tsx
@@ -12,11 +12,7 @@ const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
-const Login: React.FC<Record<string, never>> = () => {
-
-  const [signedIn, setSignIn] = useState(false);
-
-  const onFinish = (values: any) => {
+ const onFinish = (values: any) => {
     console.log("Success:", values);
   };
 
@@ -24,59 +20,87 @@ const Login: React.FC<Record<string, never>> = () => {
     console.log("Failed:", errorInfo);
   };
 
-  const renderView = () => {
-    if 
+  const renderView = (signedIn: boolean, setSignIn: any) => {
+    if (signedIn) {
+      return (
+        <div>
+          <h1>
+            hello world
+          </h1>
+          <Button onClick={() => {setSignIn(!signedIn)}}>
+            Toggle View
+          </Button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Logo page="Login To VUMS"></Logo>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            position: "relative",
+            left: "-60px",
+            paddingTop: "40px",
+          }}
+        >
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            style={{ width: "400px" }}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: "Please input your username!" }]}
+            >
+              <Input />
+            </Form.Item>
+  
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: "Please input your password!" }]}
+            >
+              <Input.Password />
+            </Form.Item>
+  
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+  
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+          <Button onClick={() => {setSignIn(!signedIn)}}>
+            Toggle View
+          </Button>
+        </div>
+        </div>
+        
+      )
+    }
   }
+
+const Login: React.FC<Record<string, never>> = () => {
+
+  const [signedIn, setSignIn] = useState(false)
+  const curView = renderView(signedIn, setSignIn); 
 
   return (
     <div>
-      <Logo page="Login To VUMS"></Logo>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          position: "relative",
-          left: "-60px",
-          paddingTop: "40px",
-        }}
-      >
-        <Form
-          {...layout}
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          style={{ width: "400px" }}
-        >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+      {
+        curView
+      }
     </div>
   );
 };
