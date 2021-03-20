ba6f5307062b4626f7bdd2796ba7cfd9580c0512
aadarshjha <jha.aadarsh@gmail.com>
2021-03-20 12:38:35 -0500

    form added

--- client/src/views/login/newArticle.tsx
+++ client/src/views/login/newArticle.tsx
@@ -2,96 +2,65 @@
 import React, { useState } from "react";
 // import { Menu, Dropdown, Button } from "antd";
 import "antd/dist/antd.css";
-import "./options.css"
+import "./options.css";
 import {
-    Form,
-    Input,
-    Cascader,
-    Select,
-    Row,
-    Col,
-    Checkbox,
-    Button,
-    AutoComplete
-  } from 'antd';
+  Form,
+  Input,
+  Button,
+} from "antd";
 
 const { TextArea } = Input;
 
-
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 8 },
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
     },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 16 },
+    sm: {
+      span: 16,
+      offset: 8,
     },
-  };
-  const tailFormItemLayout = {
-    wrapperCol: {
-      xs: {
-        span: 24,
-        offset: 0,
-      },
-      sm: {
-        span: 16,
-        offset: 8,
-      },
-    },
-  };
-
+  },
+};
 
 const NewArticle: React.FC<Record<string, never>> = () => {
-    const [form] = Form.useForm();
+  const [form] = Form.useForm();
 
-    const onFinish = (values: any) => {
-      console.log('Received values of form: ', values);
-    };
-  
-    // const prefixSelector = (
-    //   <Form.Item name="prefix" noStyle>
-    //     <Select style={{ width: 70 }}>
-    //       <Option value="86">+86</Option>
-    //       <Option value="87">+87</Option>
-    //     </Select>
-    //   </Form.Item>
-    // );
-  
-    const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([]);
-  
-    const onWebsiteChange = (value: string) => {
-      if (!value) {
-        setAutoCompleteResult([]);
-      } else {
-        setAutoCompleteResult(['.com', '.org', '.net'].map(domain => `${value}${domain}`));
-      }
-    };
-  
-    const websiteOptions = autoCompleteResult.map(website => ({
-      label: website,
-      value: website,
-    }));
-  
-    return (
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <div className="centeredForm">
       <Form
         {...formItemLayout}
         form={form}
         name="register"
         onFinish={onFinish}
         initialValues={{
-          residence: ['zhejiang', 'hangzhou', 'xihu'],
-          prefix: '86',
+          residence: ["zhejiang", "hangzhou", "xihu"],
+          prefix: "86",
         }}
         scrollToFirstError
-      >  
+      >
         <Form.Item
           name="article"
           label="Article"
           rules={[
             {
               required: true,
-              message: 'Enter Article ID',
+              message: "Enter Article ID",
             },
           ]}
           hasFeedback
@@ -105,7 +74,7 @@ const NewArticle: React.FC<Record<string, never>> = () => {
           rules={[
             {
               required: true,
-              message: 'Enter Article Title',
+              message: "Enter Article Title",
             },
           ]}
           hasFeedback
@@ -113,27 +82,28 @@ const NewArticle: React.FC<Record<string, never>> = () => {
           <Input />
         </Form.Item>
 
-
         <Form.Item
           name="body"
           label="Body"
           rules={[
             {
               required: true,
-              message: 'Enter Article Body',
+              message: "Enter Article Body",
             },
           ]}
           hasFeedback
         >
           <TextArea rows={4} />
         </Form.Item>
-   
+
         <Form.Item {...tailFormItemLayout}>
           <Button type="primary" htmlType="submit">
-            Register
+            Submit Article
           </Button>
         </Form.Item>
       </Form>
-    )};
+    </div>
+  );
+};
 
 export default NewArticle;
--- client/src/views/login/options.tsx
+++ client/src/views/login/options.tsx
@@ -3,66 +3,57 @@ import React, { useState } from "react";
 import { Menu, Dropdown, Button } from "antd";
 import { DownOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
-import "./options.css"
-import NewArticle from "./newArticle"
-
+import "./options.css";
+import NewArticle from "./newArticle";
 
 const updateView = (updatedView: string) => {
   // console.log(updatedView)
   if (updatedView == "new") {
-    return (
-      <NewArticle />
-    )
+    return <NewArticle />;
   } else if (updatedView == "update") {
-    return (
-      <h1>
-        Test
-      </h1>
-    )
+    return <h1>Test</h1>;
   } else {
-    return (
-      <h1>
-        Bye
-      </h1>
-    )
+    return <h1>Bye</h1>;
   }
- }
+};
 
 const Options: React.FC<Record<string, never>> = () => {
-  // tells us which option were on: 
-  const [form, updateForm] = useState("new")
+  // tells us which option were on:
+  const [form, updateForm] = useState("new");
 
   const menu = (
     <Menu>
-      <Menu.Item onClick={() => {updateForm("new")}}>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <Menu.Item
+        onClick={() => {
+          updateForm("new");
+        }}
+      >
+        <a target="_blank" rel="noopener noreferrer">
           New Article
         </a>
       </Menu.Item>
-      <Menu.Item onClick={() => {updateForm("update")}}>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <Menu.Item
+        onClick={() => {
+          updateForm("update");
+        }}
+      >
+        <a target="_blank" rel="noopener noreferrer">
           Update Article
         </a>
       </Menu.Item>
-      <Menu.Item onClick={() => {updateForm("delete")}}>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <Menu.Item
+        onClick={() => {
+          updateForm("delete");
+        }}
+      >
+        <a target="_blank" rel="noopener noreferrer">
           Delete Article
         </a>
       </Menu.Item>
     </Menu>
   );
 
-  const updatedView = updateView(form)
-  
+  const updatedView = updateView(form);
 
   return (
     <div className="centered">
@@ -71,9 +62,7 @@ const Options: React.FC<Record<string, never>> = () => {
           Options <DownOutlined />
         </Button>
       </Dropdown>
-      {
-        updatedView
-      }
+      {updatedView}
     </div>
   );
 };
