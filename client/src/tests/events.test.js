f96e8cb566c6d6c440671c55dd2e62dc6f84d9c6
aadarshjha <jha.aadarsh@gmail.com>
2021-04-01 22:16:46 -0500

    updates testing docs

--- client/src/tests/events.test.js
+++ client/src/tests/events.test.js
@@ -3,30 +3,35 @@ import ReactDOM from "react-dom";
 import Events from "../views/events/events";
 import renderer from "react-test-renderer";
 
+// ensures that the events.tsx can itself render. 
 it("renders events.tsx without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Events />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
+// checks if the <Logo /> component's img works. 
 it("renders the VUMS logo", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Events />, div);
   expect(div.querySelector(".vumslogo")).toBeTruthy();
 });
 
+// checks if the <Logo /> component's h1 title page descriptor works. 
 it("renders the correct title of the page", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Events />, div);
   expect(div.querySelector("h1").textContent).toBe("Events With VUMS");
 });
 
+// checks if the default event card can load. s
 it("loads a default card for testing pruposes", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Events />, div);
   expect(div.querySelector(".ant-card")).toBeTruthy();
 });
 
+// checks if the correct event title is rendered for the default card. 
 it("loads the correct testing event title", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Events />, div);
@@ -35,6 +40,7 @@ it("loads the correct testing event title", () => {
   );
 });
 
+// checks if the correct event description is rendered for the default card. 
 it("loads the correct testing event description", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Events />, div);
@@ -42,6 +48,9 @@ it("loads the correct testing event description", () => {
     "This is a simple test card for the purpose of demonstration for JEST testing in react. It will be removed!"
   );
 });
+
+// checks if the current iteration of the page matches a recent
+// UI snapshot. 
 it("matches events page snapshot", () => {
   const tree = renderer.create(<Events />).toJSON();
   expect(tree).toMatchSnapshot();
--- client/src/views/events/events.tsx
+++ client/src/views/events/events.tsx
@@ -9,17 +9,21 @@ const { Meta } = Card;
 const Events: React.FC<Record<string, never>> = () => {
   const [eventData, setEvents] = useState([]);
 
+  // fetches mongoDB data from the backend.
   useEffect(() => {
     axios.get("http://localhost:5000/events").then((res) => {
       setEvents(res.data);
     });
   }, []);
   return (
+    // returns, dynamically, all the events formatted as cards.
     <div className="centeredCards">
       <Logo page="Events With VUMS" />
 
+      {/* a map function will take all the fetched data and created Card objects */}
       {eventData.map((element: any) => {
         return (
+          // Card objects will represent the data through a unique UI.
           <Card
             hoverable
             key={element.title}
@@ -30,6 +34,7 @@ const Events: React.FC<Record<string, never>> = () => {
           </Card>
         );
       })}
+      {/* A constant card that exists for testing purposes, will be removed in production. */}
       <Card
         hoverable
         style={{ width: "40%", marginBottom: "10px" }}
