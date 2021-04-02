b472acb1a112a257ed4fa71f70c29abc1d84a384
aadarshjha <jha.aadarsh@gmail.com>
2021-04-01 22:00:29 -0500

    updates package.json for js files

--- client/src/tests/events.test.js
+++ client/src/tests/events.test.js
@@ -1,49 +1,52 @@
-// Test the Events component 
-import { getByTestId } from '@testing-library/react';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {render} from 'react-dom';
-import Events from '../views/events/events';
-import Logo from '../components/Logo';
-import renderer from 'react-test-renderer';
+// Test the Events component
+import { getByTestId } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import Events from "../views/events/events";
+import Logo from "../components/Logo";
+import renderer from "react-test-renderer";
 
 it("renders events.tsx without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Events />, div); 
-    ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement("div");
+  ReactDOM.render(<Events />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
 
 it("renders the VUMS logo", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Events />, div); 
-    expect(div.querySelector(".vumslogo")).toBeTruthy();
-})
+  const div = document.createElement("div");
+  ReactDOM.render(<Events />, div);
+  expect(div.querySelector(".vumslogo")).toBeTruthy();
+});
 
 it("renders the correct title of the page", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Events />, div); 
-    expect(div.querySelector('h1').textContent).toBe("Events With VUMS")
+  const div = document.createElement("div");
+  ReactDOM.render(<Events />, div);
+  expect(div.querySelector("h1").textContent).toBe("Events With VUMS");
 });
 
 it("loads a default card for testing pruposes", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Events />, div); 
-    expect(div.querySelector('.ant-card')).toBeTruthy()
+  const div = document.createElement("div");
+  ReactDOM.render(<Events />, div);
+  expect(div.querySelector(".ant-card")).toBeTruthy();
 });
 
 it("loads the correct testing event title", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Events />, div); 
-    expect(div.querySelector('.ant-card-meta-title').textContent).toBe("This is a Test Card That Will Be Removed From Production")
+  const div = document.createElement("div");
+  ReactDOM.render(<Events />, div);
+  expect(div.querySelector(".ant-card-meta-title").textContent).toBe(
+    "This is a Test Card That Will Be Removed From Production"
+  );
 });
 
 it("loads the correct testing event description", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Events />, div); 
-    expect(div.querySelector('.ant-card-meta-description').textContent).toBe("This is a simple test card for the purpose of demonstration for JEST testing in react. It will be removed!")
+  const div = document.createElement("div");
+  ReactDOM.render(<Events />, div);
+  expect(div.querySelector(".ant-card-meta-description").textContent).toBe(
+    "This is a simple test card for the purpose of demonstration for JEST testing in react. It will be removed!"
+  );
 });
 it("matches events page snapshot", () => {
-    const tree = renderer.create(<Events />).toJSON();
-    expect(tree).toMatchSnapshot(); 
-})
-
+  const tree = renderer.create(<Events />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
