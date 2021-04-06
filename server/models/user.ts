51ba4721c64b52b13f86290b27b00c3bced48f38
zmoustafa <zeyadmoustafa10@gmail.com>
2021-04-06 01:34:08 -0400

    Added some backend authentication files

---
+++ server/models/user.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    id: { type: String },
+});
+
+export default mongoose.model("User", userSchema);
