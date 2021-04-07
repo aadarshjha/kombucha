3c6d4e5e7b73862e44d5f1779b5b0900a1409e71
aadarshjha <jha.aadarsh@gmail.com>
2021-04-07 00:29:05 -0500

    some random changes

--- client/src/views/learn/learn.tsx
+++ client/src/views/learn/learn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../../components/Logo";
 import Category from "./category";
 import "./learn.css";
@@ -6,6 +6,7 @@ import "./articles.css";
 import Articles from "./articles";
 import { Button } from "antd";
 import mockData from "./mockData.json";
+import axios from "axios";
 
 type stateObject = {
   isLearning: boolean;
@@ -17,14 +18,20 @@ type stateObject = {
 };
 
 type backendData = {
-  header: string;
-  body: string;
+  author: any;
+  content: string;
+  dateUpdated: string;
   difficulty: string;
-  category: string;
+  title: string;
+  topic: any;
+  id: string;
 };
 
 // Please disregard lines 25 - 398 from the total line count.
-const dataFromBackend: Array<backendData> = mockData;
+axios.get('localhost:5000/learn/articles').then((response) => {
+  console.log(response.data)
+})
+const dataFromBackend: Array<any> = mockData;
 
 export const fetchCategories = () => {
   const easy: Array<backendData> = [];
@@ -69,13 +76,15 @@ const userView = (viewState: stateObject) => {
       fetchArticles(viewState.articleCategory, viewState.articleDifficulty)
     );
     return (
-      <Articles
-        articles={fetchArticles(
-          viewState.articleCategory,
-          viewState.articleDifficulty
-        )}
-        state={viewState}
-      />
+      // <Articles
+      //   articles={fetchArticles(
+      //     viewState.articleCategory,
+      //     viewState.articleDifficulty
+      //   )}
+      //   state={viewState}
+      // />
+      <>
+      </>
     );
   } else {
     return (
@@ -84,23 +93,23 @@ const userView = (viewState: stateObject) => {
         <div className="categories">
           {/* spread the state when we click the button */}
           {/* easy */}
-          <Category
+          {/* <Category
             difficulty={"easy"}
             categories={data.easy}
             state={viewState}
-          />
+          /> */}
           {/* medium */}
-          <Category
+          {/* <Category
             difficulty={"medium"}
             categories={data.medium}
             state={viewState}
-          />
+          /> */}
           {/* hard */}
-          <Category
+          {/* <Category
             difficulty={"hard"}
             categories={data.hard}
             state={viewState}
-          />
+          /> */}
         </div>
       </div>
     );
@@ -108,6 +117,17 @@ const userView = (viewState: stateObject) => {
 };
 
 const Learn: React.FC<Record<string, never>> = () => {
+
+  useEffect(() => {
+    // localhost is the server which we use in development
+    // we will use heroku services in deployment.
+    axios.get("http://localhost:5000/learn/articles").then((res) => {
+      // setEvents(res.data);
+      console.log(res.data)
+    });
+  }, []);
+
+
   // is learning will see if the user clicks on the category
   const [isLearning, setLearning] = useState(false);
 
