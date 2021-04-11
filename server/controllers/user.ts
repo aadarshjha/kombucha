8612d355b9793afbea4860724d5956ddbffa43b7
ashwinkumargb <ashwinkumargb@gmail.com>
2021-04-10 22:19:58 -0500

    sign in kinda works

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -13,24 +13,22 @@ interface controller {
     ): void;
   }
 
-export const signin: controller = async (req: any, res: any, next: any) => {
+export const signin: controller = async (req: any, res: any) => {
     const { username, password } = req.body;
     //console.log("Im Here", username, password);
     //res.status(200).json({ result: username , token: "123123"});
     try {
-        //if (await User.findOne({username: username}))
         const oldUser = await User.findOne({username: username});
 
-        //if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+        if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
 
-        // const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+        const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
-        // if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
 
-        // const token = jwt.sign({ username: oldUser.username, id: oldUser._id }, secret, { expiresIn: "1h" });
+        const token = jwt.sign({ username: oldUser.username, id: oldUser._id }, secret, { expiresIn: "1h" });
 
-
-        res.status(200).json({ result: oldUser, password: password, next: next, token: "1213123" });
+        res.status(200).json({ result: oldUser, token });
     } catch (err) {
         res.status(500).json({ message: "Something went wrong" });
     }
