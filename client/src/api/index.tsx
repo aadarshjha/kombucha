0ce71cdf43978c47cd65fe3064ff767353851ea3
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-04-13 01:41:06 -0500

    production link to heroku

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://enigmatic-beach-56036.herokuapp.com/"
+    : "http://localhost:5000";
+
+const API = axios.create({ baseURL: URL });
 
 export const signIn = (formData: any) => API.post("/user/signin", formData);
 export const signUp = (formData: any) => API.post("/user/signup", formData);
