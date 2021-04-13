44592cbfbd7189c6cd1d567ec88bb22f78e0e2ec
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-04-13 02:34:19 -0500

    oops frontend can't see process env

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const URL =
-  process.env.NODE_ENV === "production"
-    ? "https://enigmatic-beach-56036.herokuapp.com/"
-    : "http://localhost:5000";
+// TODO: make this read from localhost if not in deployment
+const URL = "https://enigmatic-beach-56036.herokuapp.com/"
 
 const API = axios.create({ baseURL: URL });
 
