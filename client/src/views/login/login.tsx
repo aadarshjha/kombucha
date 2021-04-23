42ac6b5cb0af18c2b0c73001ac5847d56ca9c97c
aadarshjha <jha.aadarsh@gmail.com>
2021-04-23 18:01:33 -0500

    stuff

--- client/src/views/login/login.tsx
+++ client/src/views/login/login.tsx
@@ -11,6 +11,7 @@ import { BACKEND_URL } from "../../api";
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
+  
 };
 const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
